12908f2dee66d607a760117023145f2359561b45
1503193571 <1503193571@qq.com>
2023-06-06 21:38:22 +0800

    first day with Git :) ver1.3 published

---
+++ function.py
@@ -0,0 +1,200 @@
+"""数据计算、处理及输出错误，每次打包之前运行一下"""
+
+import base64
+import os
+
+import numpy as np
+from PyQt5.QtWidgets import QMessageBox
+from scipy import stats, integrate
+
+
+def picture2py(py_name):
+    """将图像文件转换为py文件"""
+
+    write_data = []
+    for picture_name in os.listdir('Image/'):
+        filename = picture_name.replace('.', '_')
+        open_pic = open(os.path.join('Image/', "%s" % picture_name), 'rb')
+        b64str = base64.b64encode(open_pic.read())
+        open_pic.close()
+        # 注意这边b64str一定要加上.decode()
+        write_data.append('%s = "%s"\n' % (filename, b64str.decode()))
+
+    f = open('%s.py' % py_name, 'w+')
+    for data in write_data:
+        f.write(data)
+    f.close()
+
+
+def getPicture(pic_code, pic_name):
+    image = open(pic_name, 'wb')
+    image.write(base64.b64decode(pic_code))
+    image.close()
+
+
+if __name__ == '__main__':
+    picture2py('image')
+
+
+def printError(err):
+    """将捕获的错误输出"""
+
+    msgbx = QMessageBox.warning(None, 'Error Warning', f'<font face="Times New Roman" size="4">{err}</font>!',
+                                QMessageBox.Ok)
+    return
+
+
+def normalize(data):
+    """数据归一化"""
+
+    normalized_data = (data - np.min(data)) / (np.max(data) - np.min(data))
+    normalized_data *= 255
+    return normalized_data
+
+
+def toAmplitude(data, sampling_times):
+    """fft后数据处理作幅值图"""
+
+    data = np.abs(np.fft.fft(data)) / sampling_times
+    data[1:len(data) // 2] *= 2
+    return data
+
+
+def fixDateLength(sampling_times):
+    """检验//操作时数据的长度奇偶性"""
+
+    if sampling_times % 2 == 1:
+        sampling_times += 1
+    return sampling_times
+
+
+def calculateTimeDomainFeatures(data):
+    """计算时域特征"""
+
+    rows, cols = data.shape
+
+    # 有量纲统计量
+    max_value = np.amax(data, axis=1)  # 最大值
+    peak_value = np.amax(abs(data), axis=1)  # 最大绝对值
+    min_value = np.amin(data, axis=1)  # 最小值
+    mean = np.mean(data, axis=1)  # 均值
+    peak_peak_value = max_value - min_value  # 峰峰值
+    mean_absolute_value = np.mean(abs(data), axis=1)  # 绝对平均值
+    root_mean_square = np.sqrt(np.sum(data ** 2, axis=1) / cols)  # 均方根值
+    square_root_amplitude = (np.sum(np.sqrt(abs(data)), axis=1) / cols) ** 2  # 方根幅值
+    variance = np.var(data, axis=1)  # 方差
+    standard_deviation = np.std(data, axis=1)  # 标准差
+    kurtosis = stats.kurtosis(data, axis=1)  # 峭度
+    skewness = stats.skew(data, axis=1)  # 偏度
+    # 无量纲统计量
+    clearance_factor = peak_value / square_root_amplitude  # 裕度指标
+    shape_factor = root_mean_square / mean_absolute_value  # 波形指标
+    impulse_factor = peak_value / mean_absolute_value  # 脉冲指标
+    crest_factor = peak_value / root_mean_square  # 峰值指标
+    kurtosis_factor = kurtosis / (root_mean_square ** 4)  # 峭度指标
+
+    features = [max_value, peak_value, min_value, mean, peak_peak_value, mean_absolute_value, root_mean_square,
+                square_root_amplitude, variance, standard_deviation, kurtosis, skewness, clearance_factor,
+                shape_factor, impulse_factor, crest_factor, kurtosis_factor]
+
+    return features
+
+
+def calculateFrequencyDomainFeatures(data, sampling_rate):
+    """计算频域特征"""
+
+    data_fft = np.fft.fft(data, axis=1)
+    m, n = data_fft.shape  # 样本个数 和 信号长度
+
+    # 傅里叶变换是对称的，只需取前半部分数据，否则由于 频率序列 是 正负对称的，会导致计算 重心频率求和 等时正负抵消
+    mag = np.abs(data_fft)[:, :n // 2]  # 信号幅值
+    freq = np.fft.fftfreq(n, 1 / sampling_rate)[:n // 2]
+
+    ps = mag ** 2 / n  # 功率谱
+
+    centroid_frequency = np.sum(freq * ps, axis=1) / np.sum(ps, axis=1)  # 重心频率
+    mean_frequency = np.mean(ps, axis=1)  # 平均频率
+    mean_square_frequency = np.sum(ps * np.square(freq), axis=1) / np.sum(ps, axis=1)  # 均方频率
+    root_mean_square_frequency = np.sqrt(mean_square_frequency)  # 均方根频率
+
+    freq_tile = np.tile(freq.reshape(1, -1), (m, 1))  # 复制 m 行
+    fc_tile = np.tile(centroid_frequency.reshape(-1, 1), (1, freq_tile.shape[1]))  # 复制 列，与 freq_tile 的列数对应
+    frequency_variance = np.sum(np.square(freq_tile - fc_tile) * ps, axis=1) / np.sum(ps, axis=1)  # 频率方差
+    frequency_standard_deviation = np.sqrt(frequency_variance)  # 频率标准差
+
+    features = [centroid_frequency, mean_frequency, root_mean_square_frequency, frequency_variance,
+                mean_square_frequency, frequency_standard_deviation]
+
+    return features
+
+
+def twoPeaks(data):
+    """双峰法求阈值"""
+
+    data = normalize(data)
+    rows, cols = data.shape
+
+    # 存储灰度直方图
+    gray_scale_hist = np.zeros([256], np.uint64)
+    for i in range(rows):
+        for j in range(cols):
+            gray_scale_hist[round(data[i][j])] += 1
+
+    # 寻找灰度直方图的最大峰值对应的灰度值
+    max_gray_scale_location = np.where(gray_scale_hist == np.max(gray_scale_hist))
+    first_peak = max_gray_scale_location[0][0]  # 灰度值
+    # 寻找灰度直方图的第二个峰值对应的灰度值
+    distance = np.zeros([256], np.float32)
+    for i in range(256):
+        distance[i] = pow(i - first_peak, 2) * gray_scale_hist[i]  # 综合考虑 两峰距离与峰值
+    max_gray_scale_location2 = np.where(distance == np.max(distance))
+    second_peak = max_gray_scale_location2[0][0]
+
+    # 找到两个峰值之间的最小值对应的灰度值，作为阈值
+    if first_peak > second_peak:  # 第一个峰值再第二个峰值的右侧
+        temp_val = gray_scale_hist[int(second_peak):int(first_peak)]
+        min_gray_scale_location = np.where(temp_val == np.min(temp_val))
+        threshold = second_peak + min_gray_scale_location[0][0] + 1
+    else:  # 第一个峰值再第二个峰值的左侧
+        temp_val = gray_scale_hist[int(first_peak):int(second_peak)]
+        min_gray_scale_location = np.where(temp_val == np.min(temp_val))
+        threshold = first_peak + min_gray_scale_location[0][0] + 1
+    return threshold
+
+
+def OSTU(data):
+    """OSTU（大津）法"""
+
+    data = normalize(data)
+    height = data.shape[0]
+    width = data.shape[1]
+    max_gray_scale = 0
+    # 遍历每一个灰度层
+    for i in range(255):
+        # 使用numpy直接对数组进行计算
+        smaller_px = data[np.where(data < i)]
+        bigger_px = data[np.where(data >= i)]
+        smaller_ratio = len(smaller_px) / (height * width)
+        bigger_ratio = len(bigger_px) / (height * width)
+        average_gray_scale_smaller = np.mean(smaller_px) if len(smaller_px) > 0 else 0
+        average_gray_scale_bigger = np.mean(bigger_px) if len(bigger_px) > 0 else 0
+        otsu = smaller_ratio * bigger_ratio * (average_gray_scale_smaller - average_gray_scale_bigger) ** 2
+        if otsu > max_gray_scale:
+            max_gray_scale = otsu
+            threshold = i
+    return threshold
+
+
+def phaseDifferenceToStrainRate(data, sr):
+    """相位差与应变率转化"""
+
+    return 11.6e-9 * sr * data
+
+
+def phaseDifferenceToStrain(data, sr):
+    """相位差与应变转化"""
+
+    data = 11.6e-9 * sr * data
+    x = data.shape[1]
+    x = np.linspace(0, x, x)
+    return integrate.cumtrapz(data, x, initial=0)
---
+++ widget.py
@@ -0,0 +1,153 @@
+"""英文版重写父类组件的类"""
+
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator, QFont
+from PyQt5.QtWidgets import QLineEdit, QLabel, QComboBox, QCheckBox, QPushButton, QRadioButton
+from pyqtgraph import PlotWidget
+
+
+class Label(QLabel):
+
+    def __init__(self, text):
+        super(Label, self).__init__(text)
+        self.setStyleSheet('font-size: 17px; font-family: "Times New Roman";')
+
+
+class PushButton(QPushButton):
+
+    def __init__(self, text):
+        super(PushButton, self).__init__(text)
+        self.setStyleSheet('font-size: 17px; font-family: "Times New Roman";')
+
+
+class LineEdit(QLineEdit):
+
+    def __init__(self):
+        super(QLineEdit, self).__init__()
+        self.setStyleSheet('font-size: 17px; font-family: "Times New Roman";')
+
+
+class OnlyNumLineEdit(LineEdit):
+    """只能输入数字和"""
+
+    def __init__(self):
+        super(OnlyNumLineEdit, self).__init__()
+        self.regex = QRegExp('[0-9]+')
+        self.validator = QRegExpValidator(self.regex)
+        self.setValidator(self.validator)
+
+
+class NumPointLineEdit(LineEdit):
+    """只能输入数字和."""
+
+    def __init__(self):
+        super(NumPointLineEdit, self).__init__()
+        self.regex = QRegExp('[0-9. ]+')
+        self.validator = QRegExpValidator(self.regex)
+        self.setValidator(self.validator)
+
+
+class ComboBox(QComboBox):
+
+    def __init__(self):
+        super(ComboBox, self).__init__()
+        self.setStyleSheet('font-size: 16px; font-family: "Times New Roman";')
+
+
+class RadioButton(QRadioButton):
+
+    def __init__(self, text):
+        super(RadioButton, self).__init__(text)
+        self.setStyleSheet('font-size: 17px; font-family: "Times New Roman";')
+
+
+class CheckBox(QCheckBox):
+
+    def __init__(self, text):
+        super(CheckBox, self).__init__(text)
+        self.setStyleSheet('font-size: 17px; font-family: "Times New Roman";')
+
+
+# class FigureCanvas(FigureCanvasQTAgg):
+#     """设置matplotlib图可以鼠标拖动和缩放"""
+#
+#     def __init__(self, figure):
+#         super(FigureCanvas, self).__init__(figure)
+#         self.mouseX = 0  # 获取鼠标按下时的坐标X
+#         self.mouseY = 0  # 获取鼠标按下时的坐标Y
+#         self.buttonPressed = False
+#
+#         self.figure.canvas.mpl_connect("button_press_event", self.buttonPressEvent)
+#         self.figure.canvas.mpl_connect("button_release_event", self.buttonReleaseEvent)
+#         self.figure.canvas.mpl_connect("motion_notify_event", self.motionNotifyEvent)
+#         self.figure.canvas.mpl_connect('scroll_event', self.scrollEvent)
+#
+#     def buttonPressEvent(self, event):
+#         """鼠标按键按下"""
+#
+#         if event.inaxes:  # 判断鼠标是否在axes内
+#             if event.button == 1:  # 判断按下的是否为鼠标左键1（右键是3）
+#                 self.buttonPressed = True
+#                 self.mouseX = event.xdata  # 获取鼠标按下时的坐标X
+#                 self.mouseY = event.ydata  # 获取鼠标按下时的坐标Y
+#
+#     def buttonReleaseEvent(self, event):
+#         """鼠标按键释放"""
+#
+#         if self.buttonPressed:
+#             self.buttonPressed = False  # 鼠标松开，结束移动
+#
+#     def motionNotifyEvent(self, event):
+#         """鼠标移动"""
+#
+#         axes = event.inaxes
+#         if axes:
+#             if self.buttonPressed:  # 按下状态
+#                 # 计算新的坐标原点并移动
+#                 # 获取当前最新鼠标坐标与按下时坐标的差值
+#                 x = event.xdata - self.mouseX
+#                 y = event.ydata - self.mouseY
+#                 # 获取当前原点和最大点的4个位置
+#                 x_min, x_max = axes.get_xlim()
+#                 y_min, y_max = axes.get_ylim()
+#
+#                 x_min = x_min - x
+#                 x_max = x_max - x
+#                 y_min = y_min - y
+#                 y_max = y_max - y
+#
+#                 axes.set_xlim(x_min, x_max)
+#                 axes.set_ylim(y_min, y_max)
+#                 self.figure.canvas.draw()  # 绘图动作实时反映在图像上
+#
+#     def scrollEvent(self, event):
+#         """鼠标滚轮事件"""
+#
+#         axes = event.inaxes
+#         x_min, x_max = axes.get_xlim()
+#         y_min, y_max = axes.get_ylim()
+#         if not hasattr(self, 'xfanwei'):
+#             self.x_range = (x_max - x_min) / 10
+#             self.y_range = (y_max - y_min) / 10
+#         if event.button == 'up':
+#             axes.set(xlim=(x_min + self.x_range, x_max - self.x_range))
+#             axes.set(ylim=(y_min + self.y_range, y_max - self.y_range))
+#         elif event.button == 'down':
+#             axes.set(xlim=(x_min - self.x_range, x_max + self.x_range))
+#             axes.set(ylim=(y_min - self.y_range, y_max + self.y_range))
+#         self.figure.canvas.draw_idle()  # 绘图动作实时反映在图像上
+
+
+class MyPlotWidget(PlotWidget):
+    """带字体的pw"""
+
+    def __init__(self, title, xlabel, ylabel, grid=False):
+        super(MyPlotWidget, self).__init__()
+        self.setTitle(f'<font face="Times New Roman" size="5">{title}</font>')
+        self.setLabel('bottom', f'<font face="Times New Roman">{xlabel}</font>')
+        self.setLabel('left', f'<font face="Times New Roman">{ylabel}</font>')
+        self.getAxis('bottom').setTickFont(QFont('Times New Roman'))
+        self.getAxis('left').setTickFont(QFont('Times New Roman'))
+        self.getAxis('left').setWidth(50)
+        if grid:
+            self.showGrid(x=True, y=True, alpha=0.2)
